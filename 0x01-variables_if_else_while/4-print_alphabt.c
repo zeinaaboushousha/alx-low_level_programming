b0db062e75765354e31b5a4fa2b633b0acceb000
zeinaaboushousha <zeinaaboushousha04@gmail.com>
2023-07-14 16:08:50 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabets except p and e
+ * Return: Always (0)
+ */
+int main(void)
+{
+char alphabet = 'a';
+while (alphabet <= 'z')
+{
+if (alphabet != 'q' && alphabet != 'e')
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
